526e6c5eb94c5e5cf0262b37733eb626ae5939db
Sachin Umashankar <sachin@dyn-160-39-232-93.dyn.columbia.edu>
2019-05-03 14:43:57 -0400

    Cleaned up main.cpp and pycode.py

--- pycode.py
+++ pycode.py
@@ -7,30 +7,24 @@ img = ImgLibrary.ImgLibrary("test.jpg")
 temp = ImgLibrary.ImgLibrary()
 
 img.grayScale().flipHorizontal().flipVertical().blur()
-
-# Edge detection is wonky
-# img.edgeDetection()
-
-# Luminance scaling algorithm is wonky
-# img.luminanceScaling(int factor)
-
 img.padding(20)
 img.save("output_padded.jpg")
-img.cropImage(20,20,1920,1080)
+
+img.cropImage(20,20,img.getWidth()-30, img.getHeight()-30)
 img.save("output_cropped.jpg")
 
-#Does not work. Deepcopy is not implemented. No idea how to.
+#Does not work. Deepcopy is not implemented.
 temp = img
 
 # temp now points to the object held by img.
 temp.masking(50,50,50,50)
 temp.save("output_masked.jpg")
+
+# both temp and imp point to same object
 img.brightnessMod(1.5).contrastMod(1.5)
 img.rotateAntiClockwise().rotateClockwise().rotate180()
 img.invert();
+
+# Notice how both output the same image, even though they're different vars
 temp.save("output_inverted_rotate_temp.jpg")
 img.save("output_inverted_rotate_img.jpg")
-
-print(img.getHeight(), img.getWidth())
-
-# somecode = cppimport.imp("ImgLibrary")
